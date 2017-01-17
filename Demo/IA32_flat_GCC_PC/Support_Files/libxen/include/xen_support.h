045fbf7294f7d9734018890fc9276ea44a33d11b
kugwa <kugwa2000@gmail.com>
2017-01-17 13:30:20 +0800

    Demonstrate grant table usage

--- Demo/IA32_flat_GCC_PC/Support_Files/libxen/include/xen_support.h
+++ Demo/IA32_flat_GCC_PC/Support_Files/libxen/include/xen_support.h
@@ -1,6 +1,7 @@
 #include <stdint.h>
 
 extern struct { char _entry[32]; } hypercall_page[];
+extern char grant_page[];
 
 void cpuid(uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx);
 void rdmsr(uint32_t msr, uint32_t *low, uint32_t *high);
