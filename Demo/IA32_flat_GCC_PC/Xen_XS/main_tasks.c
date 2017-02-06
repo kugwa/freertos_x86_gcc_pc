7bdb52b001afa83d13a3919f80254780616b5863
kugwa <kugwa2000@gmail.com>
2017-02-06 13:49:00 +0800

    Get dom and ref from Xenstore

--- Demo/IA32_flat_GCC_PC/Xen_XS/main_tasks.c
+++ Demo/IA32_flat_GCC_PC/Xen_XS/main_tasks.c
@@ -5,13 +5,62 @@
 #include "pc_support.h"
 #include "xen_support.h"
 
+#include "public/xen.h"
+#include "public/grant_table.h"
+
+static int atoi(char *s)
+{
+	int slen = 0;
+	while( s[slen] != '\0' ) slen++;
+	
+	int res = 0;
+	int temp = 1;
+	int i = slen-1;
+	for( ; i >= 0; i-- ){
+		res += ((s[i] - 48)*temp) ;
+		temp *= 10;
+	}
+		
+	return res;
+}
+
 static void prvLoopTask( void *pvParameters )
 {
+    struct gnttab_map_grant_ref map_op;
+    char ref[5] = {0}, dom[5] = {0};
+    int ret;
+
     if (xen_support_init() != 0) goto loop;
 
-    char ret_value[128] = {0};
-    xenstore_read("domid", ret_value, 128);
-    printf("%s: xenstore_read(\"domid\") = %s\n", __func__, ret_value);
+    /* Get domW's dom and ref */
+    do {
+        ret = xenstore_read("data/nex/remote-gref", ref, 3);
+    } while( ret < 0 || ref[0] == 0 );
+    do {
+        ret = xenstore_read("data/nex/remote-dom", dom, 3);
+    } while( ret < 0 || dom[0] == 0 );
+
+    /* Map domW's page */
+    map_op.host_addr = (uint32_t)grant_page;
+    map_op.flags = GNTMAP_host_map;
+    map_op.dom = atoi(dom);
+    map_op.ref = atoi(ref);
+    ret = hypercall(__HYPERVISOR_grant_table_op, GNTTABOP_map_grant_ref, \
+                    (uint32_t)(&map_op), 1, 0, 0, 0);
+    if(ret != 0 || map_op.status != GNTST_okay) {
+        printf("%s: __HYPERVISOR_grant_table_op() = %d, " \
+               "dom = %d, ref = %d\n", \
+               __func__, ret, map_op.dom, map_op.ref);
+        goto loop;
+    }
+
+    printf("%s: grant_page[] = \"%s\"\n", __func__, &grant_page[28]);
+    printf("%s: strcpy(grant_page, \"Bob\")\n", __func__);
+    grant_page[28] = 'B';
+    grant_page[29] = 'o';
+    grant_page[30] = 'b';
+    grant_page[31] = '\0';
+	
 
 loop:
     (void)pvParameters;
@@ -23,7 +72,8 @@ loop:
 
 void main_tasks( void )
 {
-    xTaskCreate( prvLoopTask, "Loop", configMINIMAL_STACK_SIZE * 2, NULL, mainLOOP_TASK_PRIORITY, NULL );
+    xTaskCreate(prvLoopTask, "Loop", configMINIMAL_STACK_SIZE * 2, NULL, \
+                mainLOOP_TASK_PRIORITY, NULL);
     vTaskStartScheduler();
     for( ;; );
 }
