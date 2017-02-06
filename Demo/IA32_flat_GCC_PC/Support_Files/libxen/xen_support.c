77cfffdbfc4c7b501f07c014df28e91b682e37aa
kugwa <kugwa2000@gmail.com>
2017-02-07 01:15:57 +0800

    Establish shared memory in xen_support_init()

--- Demo/IA32_flat_GCC_PC/Support_Files/libxen/xen_support.c
+++ Demo/IA32_flat_GCC_PC/Support_Files/libxen/xen_support.c
@@ -2,10 +2,31 @@
 #include "pc_support.h"
 #include "xen_support.h"
 
+#include "public/xen.h"
+#include "public/grant_table.h"
+
+static int atoi(char *s)
+{
+	int slen = 0;
+	while( s[slen] != '\0' ) slen++;
+
+	int res = 0;
+	int temp = 1;
+	int i = slen-1;
+	for( ; i >= 0; i-- ){
+		res += ((s[i] - 48)*temp) ;
+		temp *= 10;
+	}
+
+	return res;
+}
+
 int xen_support_init()
 {
     uint32_t base;
-    int ret;
+    int i, ret;
+    struct gnttab_map_grant_ref map_op;
+    char ref[5], dom[5];
 
     base = xen_cpuid_base();
     if (base == 0) {
@@ -17,6 +38,30 @@ int xen_support_init()
     ret = xenstore_init();
     if (ret != 0) {
         printf("%s: Failed to initialize Xenstore.\n", __func__);
+        return -1;
+    }
+
+    /* Get domW's dom and ref */
+    do {
+        for (i = 0; i < 5; i++) ref[i] = 0;
+        ret = xenstore_read("data/nex/remote-gref", ref, 3);
+    } while( ret < 0 || ref[0] == 0 );
+    do {
+        for (i = 0; i < 5; i++) dom[i] = 0;
+        ret = xenstore_read("data/nex/remote-dom", dom, 3);
+    } while( ret < 0 || dom[0] == 0 );
+
+    /* Map domW's page */
+    map_op.host_addr = (uint32_t)grant_page;
+    map_op.flags = GNTMAP_host_map;
+    map_op.dom = atoi(dom);
+    map_op.ref = atoi(ref);
+    ret = hypercall(__HYPERVISOR_grant_table_op, GNTTABOP_map_grant_ref, \
+                    (uint32_t)(&map_op), 1, 0, 0, 0);
+    if(ret != 0 || map_op.status != GNTST_okay) {
+        printf("%s: __HYPERVISOR_grant_table_op() = %d, " \
+               "dom = %d, ref = %d\n", \
+               __func__, ret, map_op.dom, map_op.ref);
         return -1;
     }
 
--- Demo/IA32_flat_GCC_PC/Xen_XS/main_tasks.c
+++ Demo/IA32_flat_GCC_PC/Xen_XS/main_tasks.c
@@ -5,64 +5,19 @@
 #include "pc_support.h"
 #include "xen_support.h"
 
-#include "public/xen.h"
-#include "public/grant_table.h"
-
-static int atoi(char *s)
-{
-	int slen = 0;
-	while( s[slen] != '\0' ) slen++;
-	
-	int res = 0;
-	int temp = 1;
-	int i = slen-1;
-	for( ; i >= 0; i-- ){
-		res += ((s[i] - 48)*temp) ;
-		temp *= 10;
-	}
-		
-	return res;
-}
-
 static void prvLoopTask( void *pvParameters )
 {
-    struct gnttab_map_grant_ref map_op;
-    char ref[5], dom[5];
-    int i, ret;
-
     if (xen_support_init() != 0) goto loop;
 
-    /* Get domW's dom and ref */
-    do {
-        for (i = 0; i < 5; i++) ref[i] = 0;
-        ret = xenstore_read("data/nex/remote-gref", ref, 3);
-    } while( ret < 0 || ref[0] == 0 );
-    do {
-        for (i = 0; i < 5; i++) dom[i] = 0;
-        ret = xenstore_read("data/nex/remote-dom", dom, 3);
-    } while( ret < 0 || dom[0] == 0 );
+    /* Print shared page */
+    vTaskDelay(pdMS_TO_TICKS(5000));
+    printf("%s\n", &grant_page[28]);
 
-    /* Map domW's page */
-    map_op.host_addr = (uint32_t)grant_page;
-    map_op.flags = GNTMAP_host_map;
-    map_op.dom = atoi(dom);
-    map_op.ref = atoi(ref);
-    ret = hypercall(__HYPERVISOR_grant_table_op, GNTTABOP_map_grant_ref, \
-                    (uint32_t)(&map_op), 1, 0, 0, 0);
-    if(ret != 0 || map_op.status != GNTST_okay) {
-        printf("%s: __HYPERVISOR_grant_table_op() = %d, " \
-               "dom = %d, ref = %d\n", \
-               __func__, ret, map_op.dom, map_op.ref);
-        goto loop;
-    }
-
-    printf("%s: grant_page[] = \"%s\"\n", __func__, &grant_page[28]);
-    printf("%s: strcpy(grant_page, \"Bob\")\n", __func__);
+	/* Overwrite shared page */
     grant_page[28] = 'B';
     grant_page[29] = 'o';
     grant_page[30] = 'b';
     grant_page[31] = '\0';
-	
 
 loop:
     (void)pvParameters;
